2b46c2d3f27afb97571b5483e65d9d06427fe170
Thuan Pham <phamtanthuan2311@gmail.com>
2022-02-23 20:40:34 +0700

    [👌Auto commit] My bad

---
+++ README.md
@@ -0,0 +1,10 @@
+# quản trị cơ sở dữ liệu
+
+## môi trường cài đặt
+
+- Manjaro Linux (gnome version latest)
+
+## notes
+
+- password: Mssqltest123
+- sqlcmd -S localhost -U SA -P Mssqltest123
---
+++ bai_1/bai_1.sql
@@ -0,0 +1,29 @@
+CREATE DATABASE PhamTanThuan
+
+SELECT Name from sys.Databases
+
+GO
+
+USE PhamTanThuan
+
+CREATE TABLE NhanVien (
+    MSNV int NOT NULL PRIMARY KEY,
+    HoTen varchar(255) NOT NULL,
+    NgaySinh varchar(255),
+    MSPB int
+);
+
+INSERT INTO NhanVien VALUES (1, 'ahihi', 11112001);
+INSERT INTO NhanVien VALUES (2, 'ah9hi', 12112003);
+INSERT INTO NhanVien VALUES (3, 'ah8hi', 13112001);
+INSERT INTO NhanVien VALUES (4, 'ah2hi', 14112001);
+INSERT INTO NhanVien VALUES (5, 'ah3hi', 15112001);
+INSERT INTO NhanVien VALUES (6, 'ahjhi', 16112001);
+INSERT INTO NhanVien VALUES (7, 'ahkhi', 17112001);
+INSERT INTO NhanVien VALUES (8, 'ahlhi', 18112001);
+INSERT INTO NhanVien VALUES (9, 'a1ihi', 19112001);
+INSERT INTO NhanVien VALUES (10, 'arihi', 21112001);
+
+GO
+
+select * from NhanVien;
