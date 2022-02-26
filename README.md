fed8e832f212dabafe028ab54bdfa24aa7c00012
Thuan Pham <phamtanthuan2311@gmail.com>
2022-02-26 19:00:50 +0700

    [👌Auto commit] breathe, =, breathe

--- README.md
+++ README.md
@@ -13,7 +13,12 @@
 ```bash
 sudo systemctl enable mssql-server.service
 sudo systemctl start mssql-server.service
-sudo systemctl status mssql-server.service
+sleep 5
+usql ms://SA:Mssqltest123@localhost
+```
+
+```bash
+# use sqlcmd instend of usql
 sqlcmd -S localhost -U SA -P Mssqltest123
 ```
 
@@ -23,3 +28,27 @@ sqlcmd -S localhost -U SA -P Mssqltest123
 sudo systemctl disable mssql-server.service
 sudo systemctl stop mssql-server.service
 ```
+
+-- Step 1 : Detach Database using following script
+USE [master]
+GO
+EXEC MASTER.dbo.sp_detach_db @dbname = N'test',
+@keepfulltextindexfile = N'true'
+GO
+-- Step 2 : Move Data files and Log files to new location
+-- Do this step and move to Step 3
+-- Step 3 : Attach Database using following script
+USE [master]
+GO
+CREATE DATABASE [test] ON
+( FILENAME = N'/home/master/repos/quan_tri_co_so_du_lieu/test_data.mdf' ),
+( FILENAME = N'/home/master/repos/quan_tri_co_so_du_lieu/test_log.ldf' )
+FOR ATTACH
+GO
+IF EXISTS ( SELECT name
+FROM MASTER.sys.databases sd
+WHERE name = N'test'
+AND SUSER_SNAME(sd.owner_sid) = SUSER_SNAME() )
+EXEC [test].dbo.sp_changedbowner @loginame=N'SA',
+@map=false
+GO
--- bai_1/bai_1.sql
+++ bai_1/bai_1.sql
@@ -13,17 +13,15 @@ CREATE TABLE NhanVien (
     MSPB int
 );
 
-INSERT INTO NhanVien VALUES (1, 'ahihi', 11112001);
-INSERT INTO NhanVien VALUES (2, 'ah9hi', 12112003);
-INSERT INTO NhanVien VALUES (3, 'ah8hi', 13112001);
-INSERT INTO NhanVien VALUES (4, 'ah2hi', 14112001);
-INSERT INTO NhanVien VALUES (5, 'ah3hi', 15112001);
-INSERT INTO NhanVien VALUES (6, 'ahjhi', 16112001);
-INSERT INTO NhanVien VALUES (7, 'ahkhi', 17112001);
-INSERT INTO NhanVien VALUES (8, 'ahlhi', 18112001);
-INSERT INTO NhanVien VALUES (9, 'a1ihi', 19112001);
-INSERT INTO NhanVien VALUES (10, 'arihi', 21112001);
-
-GO
+INSERT INTO NhanVien VALUES (1, 'ahihi', 11112001, 1);
+INSERT INTO NhanVien VALUES (2, 'ah9hi', 12112003, 1);
+INSERT INTO NhanVien VALUES (3, 'ah8hi', 13112001, 1);
+INSERT INTO NhanVien VALUES (4, 'ah2hi', 14112001, 1);
+INSERT INTO NhanVien VALUES (5, 'ah3hi', 15112001, 1);
+INSERT INTO NhanVien VALUES (6, 'ahjhi', 16112001, 1);
+INSERT INTO NhanVien VALUES (7, 'ahkhi', 17112001, 1);
+INSERT INTO NhanVien VALUES (8, 'ahlhi', 18112001, 1);
+INSERT INTO NhanVien VALUES (9, 'a1ihi', 19112001, 1);
+INSERT INTO NhanVien VALUES (10, 'arihi', 21112001, 1);
 
 select * from NhanVien;
